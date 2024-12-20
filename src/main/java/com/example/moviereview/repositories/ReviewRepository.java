c6e1325a1822c073fbc70aa4bd8363782fd6328c
A-TEA47 <98877024+A-TEA47@users.noreply.github.com>
2024-12-20 18:54:30 +0600

    ADD

--- src/main/java/com/example/moviereview/repositories/ReviewRepository.java
+++ src/main/java/com/example/moviereview/repositories/ReviewRepository.java
@@ -1,11 +1,8 @@
 package com.example.moviereview.repositories;
 
-import com.example.moviereview.models.Movie;
 import com.example.moviereview.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.moviereview.repositories.MovieRepository;
 
-import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
